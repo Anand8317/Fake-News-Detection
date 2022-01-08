53b11248430fc9a5595e021e1915989fcf9f9cbd
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-01-09 00:41:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 from nltk.stem.porter import PorterStemmer
-import nltk
-nltk.download('stopwords')
 from nltk.corpus import stopwords
 import pickle
 import re
